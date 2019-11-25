b321d4703f507eac1201049ce66a81566aab7793
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-25 14:53:15 +0000

    added seeding functionality for articlesData

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -14,14 +14,9 @@ exports.seed = function(knex) {
   return Promise.all([topicsInsertions, usersInsertions])
     .then(() => {
       const formattedArticles = formatDates(articleData);
-      /* 
-      
-      Your article data is currently in the incorrect format and will violate your SQL schema. 
-      
-      You will need to write and test the provided formatDate utility function to be able insert your article data.
-
-      Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-      */
+      knex("articles")
+        .insert(formattedArticles)
+        .returning("*");
     })
     .then(articleRows => {
       /* 
