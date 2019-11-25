74bcf22c4bcdf0685d3c990319a082d5c46239e1
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-25 14:31:04 +0000

    added test for correctly formatted date on single item array (util function formatDates)

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -34,6 +34,20 @@ describe.only("formatDates", () => {
     const input = [1, 2, 3];
     expect(formatDates(input)).to.not.equal(input);
   });
+  it("Returns an array with an object containing a newly formatted date", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = formatDates(input);
+    expect(actual[0].created_at).to.equal(new Date(actual[0].created_at));
+  });
 });
 
 describe("makeRefObj", () => {});
