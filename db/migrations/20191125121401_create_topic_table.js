787d919551915ba2d17a7f0cd8947908d6d6d032
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-25 12:31:33 +0000

    added functionality to create topics table and drop topics table using migrations

--- db/migrations/20191125121401_create_topic_table.js
+++ db/migrations/20191125121401_create_topic_table.js
@@ -1,7 +1,12 @@
 exports.up = function(knex) {
   console.log("Creating Topics Table");
+  return knex.schema.createTable("topics", topicsTable => {
+    topicsTable.string("slug").primary();
+    topicsTable.string("description");
+  });
 };
 
 exports.down = function(knex) {
   console.log("Dropping Topics Table");
+  return knex.schema.dropTable("topics");
 };
