9a0f9ad0e8d5621a992ff22287ebd9faf3d14854
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-25 14:10:13 +0000

    added util test for formatDates

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -7,7 +7,26 @@ const {
 
 describe.only("formatDates", () => {
   it("The function returns an array", () => {
-    expect(formatDates([])).to.deep.equal([]);
+    expect(formatDates([])).to.be.an("array");
+  });
+  it("Returns an array containing an object with the same keys as the original object", () => {
+    const input = {
+      title: "Living in the shadow of a great man",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "I find this existence challenging",
+      created_at: 1542284514171,
+      votes: 100
+    };
+    const actual = formatDates(input);
+    expect(actual[0]).keys(
+      "title",
+      "topic",
+      "author",
+      "body",
+      "created_at",
+      "votes"
+    );
   });
 });
 
