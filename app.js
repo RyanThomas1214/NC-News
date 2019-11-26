433d2f8faf5c765660289b655d4eb62650565eba
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-26 11:33:28 +0000

    added error handling for status 500

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
-const { handle404s } = require("./errors");
+const { handle404s, handle500s } = require("./errors");
 
 app.use("/api", apiRouter);
+
+// error handling middleware
+app.use(handle500s);
+
+// error controllers
 app.use("/*", handle404s);
 
 module.exports = app;
--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,9 @@
+// error handling middleware
+
+exports.handle500s = (err, req, res, next) => {
+  res.status(500).send({ msg: "Server error" });
+};
+
 // error controllers
 
 exports.handle404s = (req, res, next) => {
