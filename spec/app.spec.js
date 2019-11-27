10830a3b76cf7db00c9722a83b43a5c517c227a5
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-27 17:07:37 +0000

    removed .only in spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -479,7 +479,7 @@ describe("/api", () => {
           });
       });
     });
-    describe.only("DELETE", () => {
+    describe("DELETE", () => {
       it("status code 204: sends no content", () => {
         return request(app)
           .del("/api/comments/1")
