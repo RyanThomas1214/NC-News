979308f4a32fc0e8537e3315abef2c484edb7a0d
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-28 11:36:31 +0000

    added heroku hosting

--- db/data/index.js
+++ db/data/index.js
@@ -3,5 +3,9 @@ const ENV = process.env.NODE_ENV || "development";
 const testData = require("./test-data");
 const developmentData = require("./development-data");
 
-const data = { test: testData, development: developmentData };
+const data = {
+  test: testData,
+  development: developmentData,
+  production: developmentData
+};
 module.exports = data[ENV];
