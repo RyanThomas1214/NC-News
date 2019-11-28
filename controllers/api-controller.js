40b2085674a875fb8518d4724de1e3eb14cf3868
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-11-28 15:52:31 +0000

    remove catch off apiController

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,8 +1,5 @@
 const endpoints = require("../endpoints.json");
 
 exports.getEndpoints = (req, res, next) => {
-  res
-    .status(200)
-    .send(endpoints)
-    .catch(next);
+  res.status(200).send(endpoints);
 };
