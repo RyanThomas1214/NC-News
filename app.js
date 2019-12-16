6192b7d89dc115876bf4209564aaf73e8f94aec4
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-12-16 11:00:42 +0000

    install and use cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/api-router");
 const {
@@ -10,6 +11,7 @@ const {
 } = require("./errors");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 
